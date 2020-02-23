ea9ff2e69407e2bc3969f410554558ffe89b1dd1
mihail <oldschool3105@gmail.com>
2020-02-23 17:01:52 +0200

    allure

--- src/test/java/firstSuites/indexPageTest/TestsOnLogo.java
+++ src/test/java/firstSuites/indexPageTest/TestsOnLogo.java
@@ -5,6 +5,8 @@ import firstSuites.pages.BasePage;
 import firstSuites.pages.indexPage.LogoElement;
 import firstSuites.pages.utils.Links;
 import firstSuites.pages.utils.Pictures;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,6 +16,8 @@ public class TestsOnLogo extends BaseTest {
     String url = Links.BASIC_URL + Links.URL_AUTOPRACTICE_INDEX_PAGE;
 
     @Test
+    @Description("test")
+    @Owner("Myhail")
     public void CheckStartPage (){ //Проверка что корневой страницей сайта является Страница индеса
         getDriver().get(url);
 
@@ -23,6 +27,8 @@ public class TestsOnLogo extends BaseTest {
     }
 
     @Test
+    @Description("test")
+    @Owner("Myhail")
     public void CheckLinkLogo () {//Проверка что корневой страницей сайта является Страница индеса
             getDriver().get(url);
 
@@ -34,8 +40,10 @@ public class TestsOnLogo extends BaseTest {
     }
 
     @Test
+    @Description("test")
+    @Owner("Myhail")
     public void CheckSizeLogo () { //Проверка размера картинки
-        getDriver().get(Links.BASIC_URL + Links.URL_CATEGORY_WOMEN_PAGE);
+        getDriver().get(Links.BASIC_URL + Links.URL_AUTOPRACTICE_INDEX_PAGE);
         BasePage basePage = new BasePage(getDriver());
         basePage.openPage(Links.URL_AUTOPRACTICE_INDEX_PAGE);
         LogoElement logoElement = new LogoElement(getDriver());
